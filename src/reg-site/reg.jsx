b8dd322f8aa33fa5fbef9cb4009e1cbf719a2ab5
User <test@test.com>
2025-07-23 18:49:31 +0300

    change form reg

--- src/reg-site/reg.jsx
+++ src/reg-site/reg.jsx
@@ -2,50 +2,59 @@ import React from "react";
 import "../reg-site/reg.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap";
+import closeImage from "../assets/close.svg";
+import { left } from "@popperjs/core";
 
 
-let change = ()=>{
+let change = () => {
    let modal = document.querySelector(".modal-window-reg");
    modal.classList.toggle("hide-reg");
    let obj = document.querySelector(".modal-window-background-reg");
    obj.classList.toggle("hide-reg");
 }
-let reg = ()=>{
-   return(<div>
-      <p><a onClick={change} className="upper-text whitesmoke" href="#">Регистрация</a></p>
+let MessageError = ()=>{
+   return <p className="error error-message-reg"></p>;
+}
+let reg = () => {
+   return (<div>
+      <p><a onClick={change} className="upper-text whitesmoke link-reg" href="#">Регистрация</a></p>
       <div className="modal-window-background-reg hide-reg">
 
       </div>
       <div className="modal-window-reg hide-reg">
          <div className="speed-reg">
-         <span >Быстрая регистрация</span>
-         <span className="close-registration" onClick={change}>Close</span>
+            <span className="bold-reg">Быстрая регистрация</span>
+            <span className="close-registration" onClick={change}>
+               <img style={{width:"20px",
+                           height:"20px" 
+               }} src={closeImage}></img>
+            </span>
          </div>
-         <hr/>
+         <hr  className="hr-reg"/>
          <form method="get" className="form-reg">
             <div>
                <label>Твой email <span className="star">*</span></label>
-               <input className="form-control" type="input" name="email"/>
-               <p className="error error-message-reg"></p>
+               <input className="form-control" type="input" name="email" />
+               <MessageError/>
             </div>
             <div>
                <label>Твой логин <span className="star">*</span></label>
-               <input className="form-control" type="input" name="email"/>
-               <p className="error error-message-reg"></p>
+               <input className="form-control" type="input" name="login" />
+               <MessageError/>
             </div>
             <div>
                <label>Пароль <span className="star">*</span></label>
-               <input className="form-control" type="input" name="email"/>
-               <p className="error error-message-reg"></p>
+               <input className="form-control" type="input" name="password" />
+               <MessageError/>
             </div>
             <div>
-               <input name="form-check-input rules-check-box" type="checkbox"/>
+               <input name="form-check-input rules-check-box" type="checkbox" />
                <label for="rules-check-box"> Я согласен с <a href="#" target="_blank">правилами сайта</a></label>
             </div>
-            <input className="btn-reg btn btn-primary" type="submit" value="Зарегистрироваться"/>
+            <input className="btn-reg btn btn-primary" type="submit" value="Зарегистрироваться" />
          </form>
-         <hr/>
-         <p>У тебя уже есть аккаунт?<a href="#">Заходи!</a></p>
+         <hr className="hr-reg"/>
+         <p>У тебя уже есть аккаунт?<a  href="#">Заходи!</a></p>
       </div>
    </div>)
 }
