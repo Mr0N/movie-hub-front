98870a3fe116a1703543e52659b3e0cb681e210c
User <test@test.com>
2025-07-27 14:36:41 +0300

    Card movies

--- src/component/card/card-site.jsx
+++ src/component/card/card-site.jsx
@@ -1,21 +1,53 @@
 import React, { useState } from "react";
 import "./card-site.css";
 import preview from "../../assets/preview.svg";
+import anime from "../../assets/card/anime.svg"
+import cartoons from "../../assets/card/cartoons.svg"
+import movies from "../../assets/card/movies.svg"
+import serials from "../../assets/card/serials.svg"
+let Anime = ()=>{
+     return  <div className="card-block">
+            <p>Аниме</p>
+        <img className="card-icon-data" src={anime}></img>
+        </div> 
+}
+let Cartoons = ()=>{
+       return  <div className="card-block">
+            <p>Мультфильмы</p>
+        <img className="card-icon-data" src={cartoons}></img>
+        </div> 
+}
+let Serials = ()=>{
+     return  <div className="card-block">
+            <p>Сериалы</p>
+        <img className="card-icon-data" src={serials}></img>
+        </div> 
+} 
+let Movies = ()=>{
+        return  <div className="card-block">
+            <p>Фильм</p>
+        <img className="card-icon-data" src={movies}></img>
+        </div> 
+}
 
-let Card = ({ backgroundImg }) => {
+
+let Card = ({ backgroundImg,BlockData}) => {
     let [objShow, setShow] = useState(false);
     let [objViewTrailer,setViewTrailer] = useState(false);
     return <div
         style={{ backgroundImage: `url(${backgroundImg})` }}
         className="card-container" onMouseEnter={()=>setShow(true)} onMouseLeave={()=>setShow(false)}>
-        <div className="card-header">1</div>
+        <div className="card-header">
+            <BlockData/>
+        </div>
         <div  className="card-trailer" onMouseLeave={()=>setViewTrailer(false)} >
-            <div class={objViewTrailer? "card-opacity" :""} onMouseEnter={()=>setViewTrailer(true)} className="card-header-icon-trailer">
+            <div className={objViewTrailer? "card-opacity" :""} onMouseEnter={()=>setViewTrailer(true)} className="card-header-icon-trailer">
                 <img className="card-icon-trailer" src={preview}></img>
             </div>
-            <div class={objViewTrailer? "card-opacity" :"card-hide"}>
+            <div class={objViewTrailer ? "card-trailer-site card-opacity" :"card-trailer-site card-hide"}>
                 <p  style={{textTransform:"upper",
-                            color:"white"}} >Смотреть трейлер</p>
+                            color:"white",
+                            fontSize:"10px"}} >Смотреть трейлер</p>
             </div>
         </div>
         <div className="card-icon">
@@ -24,4 +56,16 @@ let Card = ({ backgroundImg }) => {
         <div className="card-footer">4</div>
     </div>
 }
-export default Card;
+let AnimeCard= ({backgroundImg})=>{
+    return <Card backgroundImg={backgroundImg} BlockData={Anime}/>
+}
+let CartoonsCard= ({backgroundImg})=>{
+    return <Card backgroundImg={backgroundImg} BlockData={Cartoons}/>
+}
+let SerialsCard= ({backgroundImg})=>{
+    return <Card backgroundImg={backgroundImg} BlockData={Serials}/>
+}
+let MoviesCard= ({backgroundImg})=>{
+    return <Card backgroundImg={backgroundImg} BlockData={Movies}/>
+}
+export  {MoviesCard,SerialsCard,AnimeCard,CartoonsCard};
--- src/component/pagination-site/pagination.jsx
+++ src/component/pagination-site/pagination.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import "./pagination.css";
-import Card from  "../card/card-site";
+import { MoviesCard, SerialsCard, AnimeCard, CartoonsCard } from "../card/card-site";
 import tempImage from "../../assets/temp-image.jpg";
-let Pagination = ()=>{
-return <div><Card backgroundImg={tempImage}/></div>
+let Pagination = () => {
+    return <div className="container">
+        <div className="row">
+            <div className="col-2">
+                <MoviesCard backgroundImg={tempImage} />
+            </div>
+            <div className="col-2">
+                <SerialsCard backgroundImg={tempImage} />
+            </div>
+            <div className="col-2">
+                <AnimeCard backgroundImg={tempImage} />
+            </div>
+            <div className="col-2">
+                <CartoonsCard backgroundImg={tempImage} />
+            </div>
+        </div>
+
+     
+    </div>
 }
 export default Pagination;
